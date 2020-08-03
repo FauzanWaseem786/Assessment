0f38e186a24d2a325b92449d8d6b5facc4fb4a3b
FauzanWaseem786 <67270902+FauzanWaseem786@users.noreply.github.com>
2020-08-03 17:57:01 +0530

    Update settings.py

--- assessment/settings.py
+++ assessment/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '*^g(3p5($+mex_hqi^ez_ccc^21l5+bg(+($_47(h9bs)&e&n6'
+SECRET_KEY = '*******************'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
